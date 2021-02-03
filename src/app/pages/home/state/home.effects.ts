40541ea6193c44689a115bba1fb59a6a23499141
Caio A. Capasso <caiocapasso@yahoo.com.br>
2021-02-03 11:51:42 -0300

    implementação das funcionalidades da SPA conforme proposto pela curso

--- src/app/pages/home/state/home.effects.ts
+++ src/app/pages/home/state/home.effects.ts
@@ -1,19 +1,44 @@
 import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import * as fromHomeActions from './home.actions'
-import { map } from 'rxjs/operators'
+
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
+import { WeatherService } from 'src/app/shared/services/weather.service';
+import { CityWeather } from 'src/app/shared/models/weather.model';
+import * as fromHomeActions from './home.actions';
 
 @Injectable()
 export class HomeEffects {
 
-  test$ = createEffect(() => this.actions$
+  loadCurrentWeather$ = createEffect(() => this.actions$
     .pipe(
-      ofType(fromHomeActions.changeText),
-      map(({ text }) => console.log(text)),
-    ), {
-    dispatch: false
-  },
+      ofType(fromHomeActions.loadCurrentWeather),
+      mergeMap(({ query }) => this.weatherService.getCityWeatherByQuery(query)),
+      catchError((err, caught$) => {
+        this.store.dispatch(fromHomeActions.loadCurrentWeatherFailed());
+        return caught$;
+      }),
+      map((entity: CityWeather) => fromHomeActions.loadCurrentWeatherSuccess({ entity })),
+    ),
   );
 
-  constructor(private actions$: Actions) { }
+  loadCurrentWeatherById$ = createEffect(() => this.actions$
+    .pipe(
+      ofType(fromHomeActions.loadCurrentWeatherById),
+      mergeMap(({ id }: { id: string }) =>
+        this.weatherService.getCityWeatherById(id)
+      ),
+      catchError((err, caught$) => {
+        this.store.dispatch(fromHomeActions.loadCurrentWeatherFailed());
+        return caught$;
+      }),
+      map((entity: CityWeather) => fromHomeActions.loadCurrentWeatherSuccess({entity})),
+    )
+  );
+
+  constructor(private actions$: Actions,
+              private store: Store,
+              private weatherService: WeatherService) {
+  }
 }
